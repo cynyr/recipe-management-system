69e2beba31a1bdbf1aee55f05336715773aff3aa
Cynyr <andrew.frink@gmail.com>
2008-07-22 09:00:39 -0500

    woo a working config file parser

--- config_parse.py
+++ config_parse.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python
 
-import os
+import os,sys
 
-#for thing in os.environ:
-    #print (thing,os.environ[thing])
 #home dir on posix at least is os.environ["HOME"]
 print os.environ["HOME"]
-config_file_path=".config/rms/config"
-try: config_file=open(os.environ["HOME"]+ config_file_path, "r")
-except Exception, e: raise
+config_file_path=".config/rms/rms.conf"
+config_dic={}
+try: 
+    config_file=open(os.environ["HOME"]+ config_file_path, "r")
+except Exception, e:
+    raise
+try:
+    config_txt=config_file.readlines()
+except Exception, e:
+    raise
+#print config_file.readlines()
+while config_txt.count("\n"):
+    config_txt.remove("\n")
+
+for line in config_txt:
+    line=line.strip("\n").strip()
+    #print line
+    #print "the first char is: " + line[0]
+    #print line[0] != "#"
+    if line[0] != "#" and line.count("="):
+        entry=line.split("=", 1)
+        #print entry
+        config_dic[entry[0]]=entry[1]
+
+print config_dic
