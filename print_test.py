4e9ffbe60ccd2c547494475ed60bd1cd5b6975a3
Andrew Frink <andrew.frink@gmail.com>
2008-08-05 16:30:31 -0500

    more printing work, page break working now

--- print_test.py
+++ print_test.py
@@ -1,37 +1,35 @@
 #!/usr/bin/env python
 import pygtk,gtk,pango
 
-def do_print(widget):
+def do_print(widget,lines):
     print_op = gtk.PrintOperation()
     settings = None
     
     if settings != None: 
         print_op.set_print_settings(settings)
     
-    lines=[]
-    for num in range(38):
-        lines.append("this is line %i\n" % num)
-#print_op.connect("begin_print", begin_print)
+    #print_op.connect("begin_print", begin_print)
     print_op.connect("draw_page", draw_page, lines)
-    print_op.set_n_pages(1)
+    print int(round((len(lines)/46)+.5))
+    print_op.set_n_pages(int(round((len(lines)/46)+.5)))
     
     res = print_op.run(gtk.PRINT_OPERATION_ACTION_PRINT_DIALOG)
     
     if res == gtk.PRINT_OPERATION_RESULT_APPLY:
         settings = print_op.get_print_settings()
-    print settings
+    #print settings
     
 def draw_page(operation, context, page_nr, user_data=None):
-        #print page_nr
+        print page_nr
         cr = context.get_cairo_context()
         width = int(context.get_width())
         height = int(context.get_height())
-        print height
+        #print height
         
-        cr.rectangle(0, 0, width, HEADER_HEIGHT)
+        #cr.rectangle(0, 0, width, HEADER_HEIGHT)
         
         #@cr.set_source_rgb(0.8, 0.8, 0.8);
-        cr.fill()
+        #cr.fill()
         
         layout = context.create_pango_layout()
         
@@ -39,18 +37,18 @@ def draw_page(operation, context, page_nr, user_data=None):
         layout.set_font_description(desc)
         
 
-        print layout.get_width(),pango.SCALE
+        #print layout.get_width(),pango.SCALE
         layout.set_width(width * pango.SCALE)
-        text_string="".join(user_data)
+        text_string="".join(user_data[page_nr*46:(page_nr+1)*46])
         layout.set_text(text_string)
         #layout.set_text("some text very very long things that may or may not get wrapped with the -1 setting.\nShould be wrapped correctly now")
         layout.set_wrap(pango.WRAP_WORD_CHAR)
         layout.set_alignment(pango.ALIGN_LEFT)
         
         x,layout_height = layout.get_size()
-        print layout_height
+        #print layout_height
         #print layout.get_line_count()
-        print layout.get_iter().get_line_yrange()
+        #print layout.get_iter().get_line_xrange()
         text_height = layout_height / pango.SCALE
         
         #cr.move_to(0,  (HEADER_HEIGHT - text_height) / 2)
@@ -64,7 +62,10 @@ HEADER_HEIGHT=0
 window=gtk.Window(gtk.WINDOW_TOPLEVEL)
 window.connect("destroy", exit)
 b=gtk.Button("Print nao!")
-b.connect("clicked", do_print)
+lines=["This is a very very very long line to test that wraping lines works, somewhat nicely\n",]
+for num in range(1,179):
+    lines.append("this is line %i\n" % num)
+b.connect("clicked", do_print, lines)
 window.add(b)
 window.show_all()
 gtk.main()
