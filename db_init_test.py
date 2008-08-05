49ffedafc11cea87ead518aba340bc80fd1b1dc7
Andrew Frink <andrew.frink@gmail.com>
2008-08-04 20:56:21 -0500

    boo

--- db_init_test.py
+++ db_init_test.py
@@ -4,6 +4,18 @@ from config_parse import ParseConfigFile
 
 def init_db(cur):
     cur.execute("""create table units (id int(5) auto_increment primary key, unit varchar(50), abbreviation varchar(20), system char(6) default "US")""")
+    #table recipes create statement.
+    cur.execute("""create table recipes (id int(10) auto_increment primary key, name varchar(50), type varchar(10), rank int(1) default 1, directions mediumtext);""")
+    #table ingredients create line.
+    cur.execute("""create table ingredients (id int(10) primary key auto_increment, ingredient varchar(75));""")
+    #table types create line.
+    cur.execute("""create table types (id int(5) primary key auto_increment, type varchar(10))""")
+    #table category create line.
+    cur.execute("""create table category (id int(10) primary key auto_increment, category varchar(15))""")
+    #table category_map
+    cur.execute("""create table category_map (id int(10) primary key auto_increment, category_id int(10))""")
+    #table ingredient_map
+    cur.execute("""create table ingredient_map (recipe_id int(10), amount varchar(75), unit_id int(10), ingredent_id int(10), notes varchar(75))""")
 
 default_options=dict(
     database_host="foo.foo.org",
@@ -11,6 +23,7 @@ default_options=dict(
     database_passwd="12345",
     database_db="foo",
 )
+
 paths=["/etc/rms/rms.conf","/home/cynyr/.config/rms/rms.conf","./rms.conf"]
 options=ParseConfigFile(paths,default_options)
 try:
@@ -18,7 +31,11 @@ try:
                                  db=options['database_db'],\
                                  user=options['database_uid'],\
                                  passwd=options['database_passwd'])
-except:
-    print "could not connect to db"
+except exception,e:
+    print exception,e
+    #print "could not connect to db"
+
 cur=con.cursor()
-cur.execute("""  """)
+print options
+#init_db(cur)
+#cur.execute("""  """)
