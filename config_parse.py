7cf3b6b26b894189ec6a3c071c396c54df476fc7
Andrew Frink <andrew.frink@gmail.com>
2008-08-11 13:24:58 -0500

    fuck roy and his config parsing ideas...

--- config_parse.py
+++ config_parse.py
@@ -25,12 +25,31 @@ def ParseConfigFile(paths,default_options={}):
                         line=line.strip("\n").strip()
                         #if (conf_line[0] != "#" or conf_line[0] != ";") and conf_line.count("="):
                         #if conf_line[0] not in comment_markers and conf_line.count("="):
+                        is_comment=False
                         for marker in comment_markers:
-                            print (marker,conf_line)
-                            if conf_line.find(marker) != 0 and conf_line.count("=") >=1:
-                                entry=conf_line.split("=", 1)
-                                print entry[1].split(marker, 1)[0].strip()
-                                default_options[entry[0]]=entry[1].split(marker, 1)[0].strip()
+                            if conf_line.strip().find(marker) == 0:
+                                is_comment=True
+                        if not is_comment:
+                            entry=conf_line.split("=", 1)
+                            value=entry[1]
+                            for marker in comment_markers:
+                                if entry[1].count(marker):
+                                    value=value.split(marker,1)[0].strip()
+                            default_options[entry[0]]=value
+                        is_comment=False
+                            #print (marker,conf_line)
+                            #print conf_line,conf_line.strip().find(marker)
+                            #if (conf_line.strip().find(marker) != 0) and conf_line.count("=") >=1:
+                                #entry=conf_line.split("=", 1)
+                                #print entry[1].split(marker, 1)[0].strip()
+                                #value=""
+                                #for mark in comment_markers:
+                                    #if mark in entry[1]:
+                                        #value=entry[1].split(mark, 1)[0].strip()
+                                #if value=="":
+                                    #value=entry[1].strip()
+                                #default_options[entry[0]]=value
+                                #value=""
     return default_options
 
 if __name__ == "__main__":
