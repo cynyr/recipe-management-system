bcd7fba4a5111fa61d67aded58e1860ba8acb07b
Andrew Frink <andrew.frink@gmail.com>
2008-08-11 13:27:22 -0500

    fuck roy and his config parsing ideas...

--- config_parse.py
+++ config_parse.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 def ParseConfigFile(paths,default_options={}):
+    """ parse a configuration file and return a dictonary
+        
+        Parse a config file using the below comment markers, and return
+        a dictionary with the option name as the key and the value as the value.
+        Trailing comments are supported, 
+    """
     comment_markers=[';','#','//','/']
     for path in paths:
         try: 
+            #try to open the file
             config_file=open(path)
         except Exception, e:
+            #if it fails print a message and try the next file
             print "could not find file: %s : skipping" % path
         else:
             try:
@@ -23,8 +31,6 @@ def ParseConfigFile(paths,default_options={}):
                         conf_line=prev_line + line.strip("\n").strip()
                         prev_line=""
                         line=line.strip("\n").strip()
-                        #if (conf_line[0] != "#" or conf_line[0] != ";") and conf_line.count("="):
-                        #if conf_line[0] not in comment_markers and conf_line.count("="):
                         is_comment=False
                         for marker in comment_markers:
                             if conf_line.strip().find(marker) == 0:
@@ -37,19 +43,6 @@ def ParseConfigFile(paths,default_options={}):
                                     value=value.split(marker,1)[0].strip()
                             default_options[entry[0]]=value
                         is_comment=False
-                            #print (marker,conf_line)
-                            #print conf_line,conf_line.strip().find(marker)
-                            #if (conf_line.strip().find(marker) != 0) and conf_line.count("=") >=1:
-                                #entry=conf_line.split("=", 1)
-                                #print entry[1].split(marker, 1)[0].strip()
-                                #value=""
-                                #for mark in comment_markers:
-                                    #if mark in entry[1]:
-                                        #value=entry[1].split(mark, 1)[0].strip()
-                                #if value=="":
-                                    #value=entry[1].strip()
-                                #default_options[entry[0]]=value
-                                #value=""
     return default_options
 
 if __name__ == "__main__":
